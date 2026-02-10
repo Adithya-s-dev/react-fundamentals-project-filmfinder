c2974eda77c251b1acd72d4c96567751dd0985b0
Adithya S <adithya.s8299@gmail.com>
2026-02-10 19:44:28 +0530

    Step1: Project setup with Vite, Step2: Created Component file structure for optimize performance, Step3: Created a Movie Card file under components where it maintains all the section inside it, Step4: Created a pages folder, Step5: Created a home component where it maps all the movie card section

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const MovieCard = ({movie}) => {
+
+    function onClickFavourite() {
+        alert('Added to favourites!')
+    }
+  return (
+    <div className='movie-card'>
+        <div className='movie-poster'>
+            <img src={movie.url} alt={movie.title}/>
+            <div className='movie-overlay'>
+                <button className='favourite-button' onClick={onClickFavourite}>
+                    ♥                                {/* ♡ */}
+                </button>
+            </div>
+        </div>
+        <div className='movie-info'>
+            <h3>{movie.title}</h3>
+            <p>{movie.releaseDate}</p>
+        </div>    
+    </div>
+  )
+}
+
+export default MovieCard;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import MovieCard from '../components/MovieCard'
+
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const movies = [
+    {
+      title: 'Inception',
+      releaseDate: '2010',
+    },
+    {
+      title: 'The Dark Knight',
+      releaseDate: '2008',
+    },
+    {
+      title: 'Interstellar',
+      releaseDate: '2014',
+    },
+  ]
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`Searching for: ${searchQuery}`)
+    setSearchQuery('')
+  }
+  return (
+    <div className="home-page">
+      <form className="search-form" onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => ( movie.title.toLowerCase().startsWith(searchQuery) &&
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Home
